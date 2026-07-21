0d6ca82597cd5c822addede9ae0945c0a40c5513
Giri2K2 <prasathg537@gmail.com>
2026-07-21 12:15:37 +0530

    Enable Analytics and Speed Insights

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,17 +1,15 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
+
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-		<Analytics />
-		<SpeedInsights />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </React.StrictMode>
 );
